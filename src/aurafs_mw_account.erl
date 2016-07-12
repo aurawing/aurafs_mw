439c5f84d0cdf0b19a60544ad8cc23c9ac317ef1
aurawing <zhangxusmall@163.com>
2016-07-12 23:39:28 +0800

    add aurafs_mw_file module

--- src/aurafs_mw_account.erl
+++ src/aurafs_mw_account.erl
@@ -9,17 +9,19 @@
 -module(aurafs_mw_account).
 -author("aurawing").
 
--define(ACCOUNT, <<"account">>).
+-define(ACCOUNT_TBL, <<"account">>).
 %% API
 -export([create_account/3]).
 
 create_account(Username, Password, Space) ->
+  Token = aurafs_mw_digest:hex(uuid:uuid1()),
   Account = #{<<"username">> => Username,
               <<"password">> => aurafs_mw_digest:md5(Password),
-              <<"token">> => aurafs_mw_digest:hex(uuid:uuid1()),
-              <<"space">> => Space, <<"create_time">> => os:timestamp()},
+              <<"token">> => Token,
+              <<"space">> => Space,
+              <<"create_time">> => os:timestamp()},
   Ret = mongoc:transaction(mongo_reg,
     fun(Worker) ->
-      mc_worker_api:insert(Worker, ?ACCOUNT, Account)
+      mc_worker_api:insert(Worker, ?ACCOUNT_TBL, Account)
     end),
   Ret.
